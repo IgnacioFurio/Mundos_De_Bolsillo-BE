a85b4fdf70bd46bf172d8cc2ab9e8081c24df23e
Ignacio Furió José <bichoifj@gmail.com>
2024-01-19 15:02:15 +0100

    create new repository

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const db = require('./db.js');
+require('dotenv').config()
+const router = require('./router')
+
+const PORT = process.env.PORT || 3000;
+
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(router);
+
+app.get('/', (req,res) => {
+    return res.send('Welcome')
+});
+
+db.then(() => {
+    app.listen(PORT, () => console.log("Server on port " + PORT));
+})
+.catch((error) => console.log(error.message));
