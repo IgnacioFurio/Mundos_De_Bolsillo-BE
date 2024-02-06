bf5fe797d43a62a7b1a083264794981c3cb7340c
Ignacio Furió José <bichoifj@gmail.com>
2024-02-06 16:21:43 +0100

    Created updateWorld controller

--- views/worldRoutes.js
+++ views/worldRoutes.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 
 router.post('/post', worldController.createWorld)
 router.get('/getall', worldController.getAllWorlds)
+router.put('/put', worldController.updateWorld)
 
 module.exports = router;
