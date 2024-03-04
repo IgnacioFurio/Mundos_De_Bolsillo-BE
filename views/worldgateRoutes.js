77fd2d798462345626d9edd500a5d744a480d46b
Ignacio Furió José <bichoifj@gmail.com>
2024-03-04 18:05:13 +0100

    chore: changed getWorldGateByGameId to a post apicall isntead off a get

--- views/worldgateRoutes.js
+++ views/worldgateRoutes.js
@@ -3,7 +3,7 @@ const worldgateController = require('../controllers/worldgateController');
 const router = require('express').Router();
 
 router.post('/post', worldgateController.createWorldGate)
-router.get('/get-by-game', worldgateController.getWorldGateByGameId)
+router.post('/get-by-game', worldgateController.getWorldGateByGameId)
 router.put('/put', worldgateController.updateWorldGate)
 router.delete('/delete', worldgateController.deleteWorldGate)
 
