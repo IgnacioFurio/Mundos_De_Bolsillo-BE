6f115ecb88c5c1e1dcea6e0adf2d19186a400572
Ignacio Furió José <bichoifj@gmail.com>
2024-03-20 18:32:46 +0100

    feat: getLocationsByWorldId controller created

---
+++ controllers/locationController.js
@@ -0,0 +1,39 @@
+const { Location } = require('../models');
+
+const locationController = {};
+
+locationController.getLocationsByWorldId = async (req,res) => {
+    try {
+        const { world_id } = req.body;
+
+        let locationByWorldId = [];
+
+        for (let i = 0; i < world_id.length; i++) {
+            console.log(world_id[i]);
+            
+            let location = await Location.findAll({ 
+                where: { world_id: world_id[i] }
+            });
+
+            locationByWorldId.push(location);
+        };
+        
+        return res.status(201).json(
+            { 
+                success: true,
+                message: 'Aquí están las Localizaciones que podéis visitar en la partida.',
+                data: locationByWorldId
+            }
+        );
+    } catch (error) {
+        return res.status(501).json(
+            { 
+                success: false,
+                message: 'Algún mago ha saboteado tú búsqueda, estamos trabajando en solucionarlo.',
+                error: error.message
+            }
+        );  
+    }
+};
+
+module.exports = locationController;
