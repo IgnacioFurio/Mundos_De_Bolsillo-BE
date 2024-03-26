fc7cd86f660b8537b076d6babf7dd8e6ed9afffd
Ignacio Furió José <bichoifj@gmail.com>
2024-03-26 16:49:02 +0100

    seeder: new seeder at locations seeders

--- seeders/20240314161324-Location.js
+++ seeders/20240314161324-Location.js
@@ -12,7 +12,7 @@ module.exports = {
       {name: "Waterdeep", world_id: 1, description: "Ciudad de los Esplendores", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
       {name: "Phandalin", world_id: 1, description: "Pequeño asentamiento cerca de las minas de Phandelver", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
       {name: "El Cráneo del Troll", world_id: 1, description: "Taberna regentada por Freya y Tuerto.", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
-      {name: "El Cráneo del Troll", world_id: 1, description: "Taberna regentada por Freya y Tuerto.", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {name: "Distrito 10", world_id: 2, description: "El décimo distrito es un lugar en expansión.Para ser gobernado eficazmente, se divide en seis recintos, cada uno del tamaño de una pequeña ciudad.", createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
       ], {});
   },
 
