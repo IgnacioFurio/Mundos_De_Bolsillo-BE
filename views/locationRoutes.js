1eb0ea17c4800e70d7d9894ee270fd56b9305dd8
Ignacio Furió José <bichoifj@gmail.com>
2024-05-14 11:32:30 +0200

    Migrations and Model amplified so the seeders have enought information

--- views/locationRoutes.js
+++ views/locationRoutes.js
@@ -2,6 +2,7 @@ const locationController = require('../controllers/locationController');
 
 const router = require('express').Router();
 
+router.post('/post', locationController.createLocation);
 router.post('/getall', locationController.getLocationsByWorldId);
 
 module.exports = router;
