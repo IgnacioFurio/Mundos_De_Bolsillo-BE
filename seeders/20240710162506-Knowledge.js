7d3e65379a71e57f898f0d7fa46989bf23b648cd
Ignacio Furió José <bichoifj@gmail.com>
2024-07-11 10:41:46 +0200

    create seeder for Cerneas character

--- seeders/20240710162506-Knowledge.js
+++ seeders/20240710162506-Knowledge.js
@@ -15,6 +15,9 @@ module.exports = {
       {title: "Tráficante de armas", description: "Tallin se dedicaba al comercio clandestino de armas y sustancias estupefacientes.", veracity: true, about_character_id: 2, told_from_character_id: null, about_location_id: null, from_location_id: null, heard_on_location_id: null, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
       {title: "Sádico asesino", description: "En el pasado Tallin era conocido por ser extremadamente sádico con sus congéneres.", veracity: true, about_character_id: 2, told_from_character_id: null, about_location_id: null, from_location_id: null, heard_on_location_id: null, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
       {title: "Amores del pasado", description: "Tallin estáa completamente enamorado de Wood Lee.", veracity: false, about_character_id: 2, told_from_character_id: null, about_location_id: null, from_location_id: null, heard_on_location_id: null, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      //Cerneas
+      {title: "Doppelganger", description: "Cerneas es en realidad un doppelganger.", veracity: true, about_character_id: 9, told_from_character_id: null, about_location_id: null, from_location_id: null, heard_on_location_id: null, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
+      {title: "Druida", description: "Cerneas puede cambiar su foram a la de un animal.", veracity: true, about_character_id: 9, told_from_character_id: null, about_location_id: null, from_location_id: null, heard_on_location_id: null, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"},
     ], {});
   },
 
