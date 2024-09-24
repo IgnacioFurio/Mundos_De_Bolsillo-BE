515e529b3dd1e893550dc4b2bf935d62d4ed339d
Ignacio Furió José <bichoifj@gmail.com>
2024-09-24 11:59:35 +0200

    feat: deleteQuest controller created

--- controllers/questController.js
+++ controllers/questController.js
@@ -143,44 +143,45 @@ questController.getQuestByCharacterId = async (req,res) => {
 //     }
 // };
 
-// questController.deleteCharacter = async (req,res) => {
-//     try {
-//         const { character_id } = req.body
+questController.deleteQuest = async (req,res) => {
+    try {
+        const { quest_id } = req.body
 
-//         const findCharacter = await Character.findByPk(character_id);
+        
+        const deleteCharacterQuest = await Characterquest.destroy(
+            {where: {quest_id: quest_id}}
+        );
+        
+        const deleteQuest = await Quest.destroy(
+            {where: {id: quest_id}}
+        );     
 
-//         if (!findCharacter) {
-//             return res.status(404).json(
-//                 { 
-//                     success: false,
-//                     message: 'No hemos encontrado registros de tal personaje.',
-//                 }
-//             ); 
-//         }
+        if ( !deleteCharacterQuest || !deleteQuest) {
+            return res.status(404).json(
+                {
+                    success: true,
+                    message: `La misión que buscas no está registrada.`,
+                }
+            ); 
+        }
 
-//         const deleteCharacter = await Character.destroy({
-//             where: {
-//                 id: character_id
-//             }
-//         });
+        return res.status(200).json(
+            {
+                success: true,
+                message: `La misión "${deleteQuest.name}" ha sido eliminada de todo registro.`,
+                data: deleteQuest
+            }
+        );  
 
-//         return res.status(200).json(
-//             {
-//                 success: true,
-//                 message: `Nos hemos desecho de ${findCharacter.name}, ahora mantendremos el secreto entre nosotros.`,
-//                 data: findCharacter
-//             }
-//         );  
-
-//     } catch (error) {
-//         return res.status(501).json(
-//             { 
-//                 success: false,
-//                 message: 'Estamos teniendo problemas para localizar al personaje.',
-//                 error: error.message
-//             }
-//         ); 
-//     }
-// };
+    } catch (error) {
+        return res.status(501).json(
+            { 
+                success: false,
+                message: 'Estamos teniendo problemas para acceder a la información de la misión.',
+                error: error.message
+            }
+        ); 
+    }
+};
 
 module.exports = questController
--- views/questRoutes.js
+++ views/questRoutes.js
@@ -5,6 +5,6 @@ const router = require('express').Router();
 router.post('/post', questController.createQuest)
 router.post('/get-by-character', questController.getQuestByCharacterId)
 // router.put('/put', questController.updatequest)
-// router.delete('/delete', questController.deletequest)
+router.delete('/delete', questController.deleteQuest)
 
 module.exports = router;
