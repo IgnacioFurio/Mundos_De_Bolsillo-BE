c595b8914a9d290899c03123a458af05218818ce
Ignacio Furió José <bichoifj@gmail.com>
2024-11-12 12:22:59 +0100

    fix: created default value for createdAT and updatedAt fields

--- seeders/20241112110056-CharacterScenes.js
+++ seeders/20241112110056-CharacterScenes.js
@@ -8,19 +8,19 @@ module.exports = {
      *
      * Example:*/
       await queryInterface.bulkInsert('CharacterScenes', [
-        { scene_id: 1, character_id: 1 },
-        { scene_id: 1, character_id: 2 },
-        { scene_id: 2, character_id: 1 },
-        { scene_id: 2, character_id: 2 },
-        { scene_id: 3, character_id: 3 },
-        { scene_id: 3, character_id: 4 },
-        { scene_id: 3, character_id: 5 },
-        { scene_id: 3, character_id: 6 },
-        { scene_id: 4, character_id: 7 },
-        { scene_id: 4, character_id: 8 },
-        { scene_id: 4, character_id: 9 },
-        { scene_id: 4, character_id: 10 },
-        { scene_id: 4, character_id: 11 },
+        { scene_id: 1, character_id: 1, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00" },
+        { scene_id: 1, character_id: 2, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 2, character_id: 1, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 2, character_id: 2, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 3, character_id: 3, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 3, character_id: 4, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 3, character_id: 5, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 3, character_id: 6, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 4, character_id: 7, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 4, character_id: 8, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 4, character_id: 9, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 4, character_id: 10, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
+        { scene_id: 4, character_id: 11, createdAt: "2023-02-22 00:00:00", updatedAt: "2023-02-22 00:00:00"  },
       ], {});
   },
 
