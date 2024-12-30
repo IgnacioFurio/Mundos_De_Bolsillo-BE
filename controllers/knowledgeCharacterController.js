40d2f14cd90838aa73eb1bc5a65f659a201c03c5
Ignacio Furió José <bichoifj@gmail.com>
2024-12-30 13:49:55 +0100

    feat (controller): create createKnowledgeCharacter

---
+++ controllers/knowledgeCharacterController.js
@@ -0,0 +1,195 @@
+const { KnowledgeCharacter, Character, World, Location, Knowledge } = require('../models');
+
+const knowledgeCharacterController = {};
+
+knowledgeCharacterController.createKnowledgeCharacter = async (req,res) => {
+    try {
+        const { knowledge_id, characters_id, session_id } = req.body;
+
+        const deleteKnowledgeCharacters = await KnowledgeCharacter.destroy({
+            where: { knowledge_id: knowledge_id}
+        });
+
+        const knowledgeCharacter = [];
+
+        for (let i = 0; i < characters_id.length; i++) {
+            knowledgeCharacter.push(await KnowledgeCharacter.create(
+                {
+                    knowledge_id: knowledge_id,
+                    character_id: characters_id[i],
+                    session_id: session_id
+                }
+            ));    
+        }
+
+        const newKnowledgeCharacter = {
+                knowledge_id: knowledge_id,
+                characters_id: characters_id,
+                session_id: session_id
+            };
+
+        return res.status(201).json(
+            { 
+                success: true,
+                message: `Hemos entregado la información a las personas pertinentes, gracias por tu confianza.`,
+                data: knowledgeCharacter
+            }
+        );
+    } catch (error) {
+        return res.status(501).json(
+            { 
+                success: false,
+                message: 'No nos está siendo posible registrar la información.',
+                error: error.message
+            }
+        );  
+    }
+};
+
+// knowledgeCharacterController.getCharacterByWorldId = async (req,res) => {
+//     try {
+//         const { world_id } = req.body;
+        
+//         let characterByWorldId = [];
+        
+//         for (let i = 0; i < world_id.length; i++) {    
+//             let character = await Character.findAll({ 
+//                 where: { world_id: world_id[i] },
+//                 include: [
+//                     {
+//                         model: World,
+//                         attributes: { exclude: ["id", "description", "createdAt", "updatedAt"]}
+//                     },
+//                     {
+//                         model: Location,
+//                         as: "fromLocation",
+//                     },
+//                     {
+//                         model: Location,
+//                         as: "lastLocationKnown",
+//                     },
+//                     {
+//                         model: Knowledge,
+//                         as: "aboutCharacter"
+//                     }
+//                 ]
+//             });
+
+//             characterByWorldId.push(character);
+//         };
+
+//         characterByWorldId.sort((a,b)=>{a - b});
+        
+//         return res.status(201).json(
+//             { 
+//                 success: true,
+//                 message: 'Aquí están los personajes con los que interactuar en la partida.',
+//                 data: characterByWorldId
+//             }
+//         );
+//     } catch (error) {
+//         return res.status(501).json(
+//             { 
+//                 success: false,
+//                 message: 'Algún mago ha saboteado tú búsqueda, estamos trabajando en solucionarlo.',
+//                 error: error.message
+//             }
+//         );  
+//     }
+// };
+
+// knowledgeCharacterController.updateCharacter = async (req,res) => {
+//     try {
+//         const { id, name, description, world_id, from_location_id, last_location_known_id } = req.body
+
+//         if (id === "") {
+//             return res.status(409).json(
+//                 { 
+//                     success: false,
+//                     message: 'No hemos sido capaces de encontrar al personaje que buscas, pero seguiremos mirando en nuestras estanterías.',
+//                 }
+//             ); 
+//         };
+        
+//         if (name === "") {
+//             return res.status(409).json(
+//                 { 
+//                     success: false,
+//                     message: '¿No has nombrado al personaje?, lo sentimos, pero no podemos permitirlo.',
+//                 }
+//             ); 
+//         };
+
+//         const updateCharacter = await Character.update(
+//             {
+//                 name: name,
+//                 description: description,
+//                 world_id: world_id,
+//                 from_location_id: from_location_id,
+//                 last_location_known_id: last_location_known_id
+//             },
+//             {
+//                 where: {id: id}
+//             }
+//         );
+
+//         return res.status(200).json(
+//             {
+//                 success: true,
+//                 message: 'Estamos procediendo a guardar tu nueva información.',
+//                 data: updateCharacter
+//             }
+//         );  
+
+//     } catch (error) {
+//         return res.status(501).json(
+//             { 
+//                 success: false,
+//                 message: `Algo esta impidiendo que podamos cambiar la información de ${updateCharacter.name}.`,
+//                 error: error.message
+//             }
+//         ); 
+//     }
+// };
+
+// knowledgeCharacterController.deleteCharacter = async (req,res) => {
+//     try {
+//         const { character_id } = req.body
+
+//         const findCharacter = await Character.findByPk(character_id);
+
+//         if (!findCharacter) {
+//             return res.status(404).json(
+//                 { 
+//                     success: false,
+//                     message: 'No hemos encontrado registros de tal personaje.',
+//                 }
+//             ); 
+//         }
+
+//         const deleteCharacter = await Character.destroy({
+//             where: {
+//                 id: character_id
+//             }
+//         });
+
+//         return res.status(200).json(
+//             {
+//                 success: true,
+//                 message: `Nos hemos desecho de ${findCharacter.name}, ahora mantendremos el secreto entre nosotros.`,
+//                 data: findCharacter
+//             }
+//         );  
+
+//     } catch (error) {
+//         return res.status(501).json(
+//             { 
+//                 success: false,
+//                 message: 'Estamos teniendo problemas para localizar al personaje.',
+//                 error: error.message
+//             }
+//         ); 
+//     }
+// };
+
+module.exports = knowledgeCharacterController
